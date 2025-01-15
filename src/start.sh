e35d62e91d1ee87df14d4aaa1f79309dc8c0d186
adnanemajdoub@gmail.com <adnanemajdoub@gmail.com>
2025-01-15 17:13:50 +0100

    remove insert_data docker service

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #insert data inside the mongodb
-python src/insert_data.py
+python src/data/static/insert_data.py &
 
 # Run fetch_data.py in the background
 python src/api/fetch_data.py &
