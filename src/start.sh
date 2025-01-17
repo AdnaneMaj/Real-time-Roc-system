fcf3cdbccd4e68504553e5fe19b3a721535100f8
adnanemajdoub@gmail.com <adnanemajdoub@gmail.com>
2025-01-17 10:54:24 +0100

    some more updates concering kafka and streamelit

--- src/start.sh
+++ src/start.sh
@@ -4,4 +4,4 @@
 python src/data/static/insert_data.py
 
 # Run app.py in the foreground
-streamlit run src/dashboard/app.py
+streamlit run src/dashboard/app.py --server.port=8501 --server.address=0.0.0.0
